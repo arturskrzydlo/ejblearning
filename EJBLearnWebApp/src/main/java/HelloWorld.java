bef75c14f2ea298a676429ffc5e62db538a937c5
Artur Skrzydło <arturskrzydlo@gmail.com>
2016-02-17 20:50:38 +0100

    Hello World file added

--- EJBLearnWebApp/src/main/java/HelloWorld.java
+++ EJBLearnWebApp/src/main/java/HelloWorld.java
@@ -4,7 +4,7 @@ public class HelloWorld{
 	public static void main(String[] args){
 		
 		
-		System.out.println("Hello in the maven world");
+		System.out.println("Hello in the maven world : )");
 		
 	}
 	
